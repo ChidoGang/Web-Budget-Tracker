f03aa69cd259790de6bdbfa960fdb533bc3b789f
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2022-01-17 14:26:57 -0500

    properly linked service worker and mani

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,63 +1,62 @@
 let db;
-
-const request = indexedDB.open("budget", 1);
-
+const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function(event) {
-   
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
-};
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+  };
 
 request.onsuccess = function(event) {
-  db = event.target.result;
-
-  
-  if (navigator.onLine) {
-    checkDatabase();
-  }
-};
-
-request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
-};
-
-function saveRecord(record) {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-
-  store.add(record);
-}
-
-
-function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-  const getAll = store.getAll();
-  
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        
-        const transaction = db.transaction(["pending"], "readwrite");
-
-        
-        const store = transaction.objectStore("pending");
-
-        
-        store.clear();
-      });
+    db = event.target.result;
+    if (navigator.onLine) {
+      uploadTransaction();
     }
   };
-}
+  
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
 
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    transactionObjectStore.add(record);
+  };
 
-window.addEventListener("online", checkDatabase)
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) { 
+        fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(serverResponse => {
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+           
+            const transaction = db.transaction(['new_transaction'], 'readwrite')
+            const transactionObjectStore = transaction.objectStore('new_transaction');
+            transactionObjectStore.clear();
+
+            alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+            console.log(err);
+            });
+        }
+    };
+
+  }
+window.addEventListener('online', uploadTransaction);
